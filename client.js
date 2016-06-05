2aeba69d94d5826b90b4e30d8dfe4fc450d01cc1
Bitterulf <s_schlender@hotmail.com>
2016-06-05 14:17:25 +0200

    Add rendering to state change

--- client.js
+++ client.js
@@ -2,6 +2,7 @@ const util = require('util');
 const EventEmitter = require('events');
 const _ = require('underscore');
 const Server = require('./server.js');
+const m = require('mithril');
 
 function Client(name, password, state, server) {
   EventEmitter.call(this);
@@ -58,10 +59,21 @@ Client.prototype.trigger = function(name, payload) {
 Client.prototype.update = function(stateKey, stateData) {
   this.state[stateKey] = stateData;
   this.emit('stateChanged', this.state);
+
+  m.render(document.body, [
+    m('h1', 'wallet'),
+    m('div', this.state.wallet.map(
+      function(entry) {
+        return m('div', entry.amount);
+      })
+    )
+  ]);
 };
 
 Client.prototype.action = function(action) {
   this.actions.push(action);
 };
+
+console.log(m);
 
 module.exports = Client;
